17978c83979a8de8a7968ce57a00c422fd1db66b
Isaias Passeggi <passeggiisaias@gmail.com>
2025-05-14 19:28:06 -0300

    Se saco los .class

--- src/ar/edu/unlam/pb2/Auto.java
+++ src/ar/edu/unlam/pb2/Auto.java
@@ -1,5 +1,28 @@
 package ar.edu.unlam.pb2;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Auto {
+
+	private List<Paquete> paquete = new LinkedList<Paquete>();
+	private List<String> destinos = new LinkedList<String>();
+	private Double volumenTotal = 0.0;
+	private Double pesoTotal = 0.0;
+
+	public boolean agregar(Paquete paquete, String destino) {
+		Boolean resultado = false;
+		Boolean compatible = false;
+		if ((!this.destinos.contains(destino) && this.destinos.size() < 3) 
+			|| (this.destinos.contains(destino))) {
+			compatible = true;
+		}
+		
+		if(paquete.calcularVolumen() + this.volumenTotal <= 2.0 && paquete.getPeso() <= 500 && this.paquete.size() < 2
+				&& this.destinos.equals(destino)) {
+			resultado=true;
+		}
+		return resultado;
+	}
 
 }
--- test/ar/edu/unlam/pb2/AutoTest.java
+++ test/ar/edu/unlam/pb2/AutoTest.java
@@ -1,6 +1,6 @@
 package ar.edu.unlam.pb2;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -8,8 +8,10 @@ import org.junit.jupiter.api.Test;
 class AutoTest {
 
 	@Test
-	void TestAuto() {
-		assertTrue(true);
+	void queAceptePaqueteValido() {
+		Auto auto = new Auto();
+		Paquete p1 = new Paquete(0.1, 0.1, 0.1, 1);
+		assertTrue(auto.agregar(p1, "Moron"));
 
 	}
 
